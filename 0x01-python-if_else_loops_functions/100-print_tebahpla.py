3f96b7133fa87a0b6221a638d3bf37310544d988
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-01 05:25:40 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print(chr(i - 32) if i in range(121, 96, -2) else chr(i), end="")
+for i in range(-122, -96):
+    if i % 2 == 0:
+        let = chr(i * -1)
+    else:
+        let = chr((i * -1) - 32)
+    print("{}".format(let), end="")
