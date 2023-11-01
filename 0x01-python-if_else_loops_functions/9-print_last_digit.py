54a935e9cead297675ed381c2bdeef46278e2b45
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-01 05:29:01 +0100

    Update 9-print_last_digit.py
    Handle cases of negative numbers

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    string = str(number)
-    for i in string:
-        pass
-    print(i, end="")
-    return i
+    if number > 0:
+        number = number % 10
+    else:
+        aux = number * -1
+        number = aux % 10
+    print(number, end="")
+    return number
