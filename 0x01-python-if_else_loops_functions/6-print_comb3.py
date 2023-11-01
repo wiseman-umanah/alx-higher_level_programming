9db3ac12aa57873a2c34ff68aa3b763f7a254a3d
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-01 05:34:23 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if j < i:
-            continue
-        elif i == j:
-            continue
-        elif i == 8 and j == 9:
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}", end=", ")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        elif i != j and j > i:
+            print("{}{}, ".format(i, j), end="")
