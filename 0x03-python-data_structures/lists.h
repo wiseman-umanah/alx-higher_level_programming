72644366871026761b8c8dd9bc078b94c5ea11ea
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-07 00:41:57 +0100

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,5 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */ 
