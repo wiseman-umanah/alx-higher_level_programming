7dfd66aef60f4558d216bc9820396a4a7eb4e54a
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-07 00:39:15 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,83 @@
-#include "lists.h" 
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+  * is_palindrome - Checks if a singly linked list is a palindrome
+  * @head: The head of the singly linked list
+  *
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+  */
 int is_palindrome(listint_t **head)
 {
-    int ispalindrome, i, temp[1000];
-    listint_t *current;
-    
-    ispalindrome = 1;
-    i = 0;
+    listint_t *start = NULL, *end = NULL;
+    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+
+    if (head == NULL)
+        return (0);
+
     if (*head == NULL)
-        return (i);
-    current = *head;
-    while (current != NULL)
+        return (1);
+    
+    start = *head;
+    len = listint_len(start);
+    len_cyc = len * 2;
+    len_list = len_cyc - 2;
+    end = *head;
+
+    for (; i < len_cyc; i = i + 2)
     {
-        temp[i] = current->n;
-        current = current->next;
-        i++;
+        if (start[i].n != end[len_list].n)
+            return (0);
+
+        len_list = len_list - 2;
     }
-    i--;
-    current = *head;
-    while (i >= 0 && current != NULL)
-    {
-        if (temp[i] != current->n)
-        {
-            ispalindrome = 0;
-            break;
-        }
-        i--;
-        current = current->next;
-    }
-    return (ispalindrome);
+
+    return (1);
+}
+
+/**
+  * get_nodeint_at_index - Gets a node from a linked list
+  * @head: The head of the linked list
+  * @index: The index to find in the linked list
+  *
+  * Return: The specific node of the linked list
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+			++iter_times;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+  * slistint_len - Counts the number of elements in a linked list
+  * @h: The linked list to count
+  *
+  * Return: Number of elements in the linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
 }
