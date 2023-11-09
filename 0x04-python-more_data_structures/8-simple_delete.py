678ad35d099189cc66e69c76804ba424f605fe17
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-09 05:29:43 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            del (a_dictionary[i])
-        else:
-            return a_dictionary
-    return a_dictionary
+    try:
+        del a_dictionary[key]
+        return (a_dictionary)
+    except KeyError:
+        return (a_dictionary)
