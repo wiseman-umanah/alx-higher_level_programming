9623689287cda52a6d690b1636f26a5700211b90
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-11-20 15:14:35 +0100

    Update 101-safe_function.py
    Fixed checker error in file

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
 	try:
 		return (fct(*args))
 	except Exception as e:
-		sys.stderr.write(f"Exception: {e}\n")
+		sys.stderr.write("Exception: {}\n".format(e))
 		return None
