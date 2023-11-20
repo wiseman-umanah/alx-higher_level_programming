12cd6423acd48803c6738a2d2be9f4d64941ab11
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-20 19:35:20 +0100

    Adjusted code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,9 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
-        return result
