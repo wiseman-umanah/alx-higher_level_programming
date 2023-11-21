f484842f52401f102bc5c46d569d229846633519
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-21 20:02:07 +0100

    Fixed pycodestyle issue and added docstring

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""This module is a class for a Square
+"""
+
+
 class Square:
+    """This square class shows how  data is initialized in a class
+    Args:
+        size (int): This is the size of the defined square that is private
+    Raises:
+        ValueError: if size is less than or equal to 0
+        TypeError: if size is not an integer
+    """
     def __init__(self, size=0):
         if isinstance(size, int):
             if size >= 0:
