506dd899b4ff87b54cd20a02300dd743b931b6e5
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-21 19:55:48 +0100

    Fixed pycodestyle issue

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module is a class for a Square
 """
+
+
 class Square:
     """This square class shows how  data is initialized in a class
-    
     Args:
-    	size (int): This is the size of the defined square that is private
+         size (int): This is the size of the defined square that is private
     """
     def __init__(self, size):
         self.__size = size
