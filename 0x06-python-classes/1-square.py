d545bbd5d2517167af164783b85be2b022fea330
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-21 19:51:35 +0100

    Added docstring to 1-squar module and update README

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This module is a class for a Square
+"""
 class Square:
+    """This square class shows how  data is initialized in a class
+    
+    Args:
+    	size (int): This is the size of the defined square
+    """
     def __init__(self, size):
         self.__size = size
