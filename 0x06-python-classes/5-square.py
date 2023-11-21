c4fa17b938d1a7c5599d15cde97baa48b1c69013
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-21 20:40:52 +0100

    Fixed pycodestyle issues in 5-square.py and added docstring

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+This module is a class for a Square
+"""
+
+
 class Square:
+    """
+    This square class shows how  data is initialized in a class
+    Args:
+        size (int): This is the size of the defined square that is private
+    Atrributes:
+        area: returns the area of the square
+    Raises:
+        ValueError: if the size is less or equal to 0
+        TypeError: if size is not an integer
+    """
     def __init__(self, size=0):
         if isinstance(size, int):
             if size >= 0:
@@ -10,9 +25,13 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
+        """this function returns the area of the square
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """prints in stdout the square with the character #
+        """
         for i in range(self.__size):
             if self.__size == 0:
                 print("")
@@ -21,6 +40,13 @@ class Square:
 
     @property
     def size(self):
+        """
+        This resets the size value if the size needs to be changed
+        Args:
+            value (int): the new value for the size of the Square
+        Raises:
+            ValueError: if the new value is not an integer
+        """
         return self.__size
 
     @size.setter
