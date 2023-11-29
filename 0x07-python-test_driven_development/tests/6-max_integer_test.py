e2a3f2eb25a00641e36054693858dd8731b5f635
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-29 23:34:59 +0100

    Updated code

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,26 +4,36 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """Suite test for max_integer function"""
+
     def test_wrongParameter(self):
         result = "hello"
         self.assertEqual(max_integer(result), "o")
+
     def test_wrongArg(self):
         result = [2, 3, "H", 4, 3]
         with self.assertRaises(TypeError):
             max_integer(result)
+
     def test_forFloat(self):
         result = [1, 2, 30.7, 4, 8, 10]
         self.assertEqual(max_integer(result), 30.7)
+
     def test_integers(self):
         result = [2, 8, 2, 4, 3]
         self.assertEqual(max_integer(result), 8)
+
     def test_emptyList(self):
         self.assertEqual(max_integer([]), None)
+
     def test_oneElement(self):
         self.assertEqual(max_integer([4]), 8)
+
     def test_max_at_beginning(self):
         self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
