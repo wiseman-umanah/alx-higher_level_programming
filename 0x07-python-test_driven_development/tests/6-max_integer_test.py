3134793697d74c8856b76cae4656611f6e89ad59
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-30 00:05:18 +0100

    Modified code

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,34 +6,39 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+
     """Suite test for max_integer function"""
 
-    def test_wrongParameter(self):
-        result = "hello"
-        self.assertEqual(max_integer(result), "o")
+    def test_max_integer(self):
+        self.assertEqual(max_integer([8, -7, 100, 3]), 100)
 
-    def test_wrongArg(self):
-        result = [2, 3, "H", 4, 3]
-        with self.assertRaises(TypeError):
-            max_integer(result)
-
-    def test_forFloat(self):
-        result = [1, 2, 30.7, 4, 8, 10]
-        self.assertEqual(max_integer(result), 30.7)
-
-    def test_integers(self):
-        result = [2, 8, 2, 4, 3]
-        self.assertEqual(max_integer(result), 8)
-
-    def test_emptyList(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_oneElement(self):
-        self.assertEqual(max_integer([4]), 8)
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([50, 51, 50, 49]), 51)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-12, -8, -2, -1]), -1)
 
     def test_max_at_beginning(self):
         self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
 
+    def test_one_number(self):
+        self.assertEqual(max_integer([1]), 1)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_string_number_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([0, 'alx'])
+
+    def test_tuple_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([10, (45, 30)])
+
+    def test_dictionary(self):
+        with self.assertRaises(KeyError):
+            max_integer({'key1': 1, 'key2': 2})
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(1)
