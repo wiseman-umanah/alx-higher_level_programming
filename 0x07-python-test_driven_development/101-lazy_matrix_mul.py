f02341c911197dc547b3d40e32f4c0c452a291b1
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-29 22:55:39 +0100

    Modified code

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
 This will print the product of two matrices to output
 >>>lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]])
@@ -9,8 +9,13 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Returns the new matrix after operation
-    Parameters: m_a and m_b, the matrices
+    Function that multiplies 2 matrices
+
+    Args: 
+    	m_a (list): matrix a
+        m_b (list): matrix b
+    Returns:
+    	the new matrix after operation
     """
     m_c = np.dot(m_a, m_b)
     return m_c
