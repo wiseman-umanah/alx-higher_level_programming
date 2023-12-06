981cf2785f6942c97be32e63c3e8d519944da9cf
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-12-06 01:35:32 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """Function to open and reads a file's content"""
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+    """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read(), end="")
+        read_data = f.read()
+        print(read_data, end='')
