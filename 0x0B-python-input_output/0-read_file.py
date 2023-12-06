c5b0658fdae14355886923e3c2e475db46e02dd3
wiseman-umanah <wisemanumanah@gmail.com>
2023-12-06 01:29:24 +0100

    Fixed code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,4 @@
 def read_file(filename=""):
     """Function to open and reads a file's content"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
