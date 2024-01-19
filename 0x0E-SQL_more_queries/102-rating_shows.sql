732448d71dc8c5bcf3024a6914dee9631e11d864
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-19 01:43:00 +0100

    Updated code

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
 FROM tv_shows
 LEFT JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.show_id
 GROUP BY tv_shows.id
-ORDER BY rating_sum ASC;
+ORDER BY rating_sum DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,1 +1,7 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT tv_genres.name, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN hbtn_0d_tvshows_rate ON tv_show_genres.show_id = hbtn_0d_tvshows_rate.show_id
+GROUP BY tv_genres.id
+ORDER BY rating_sum DESC;
