e5d82bb82404a06f2721346978cc269fb70f96a3
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-19 01:40:50 +0100

    Updated code

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,6 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
+FROM tv_shows
+LEFT JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.show_id
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC;
