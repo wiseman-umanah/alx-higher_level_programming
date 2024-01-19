07665b6b47241a3bb03603532071bac48f042659
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-19 01:38:37 +0100

    Updated code

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,11 @@
 -- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+CREATE TEMPORARY TABLE temp_comedy_shows AS
+SELECT show_id
+FROM tv_show_genres
+WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy');
+
+-- Select all shows without the genre 'Comedy'
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (SELECT show_id FROM temp_comedy_shows)
+ORDER BY tv_shows.title ASC;
