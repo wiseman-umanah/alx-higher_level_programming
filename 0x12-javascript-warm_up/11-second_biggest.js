3d362adeaa86e8cec03c7be7da5b31879a5cf600
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-12 13:52:26 +0100

    Fixed code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) { console.log(0); } else {
   let temp = process.argv;
-  temp = temp.slice(2).sort();
+  temp = temp.slice(2).sort((a, b) => a - b);
   console.log(Number(temp[temp.length - 2]));
 }
