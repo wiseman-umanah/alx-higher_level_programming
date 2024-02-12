a7c96495e5dc0497053caa3a6bc3297c1c845b9e
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-12 13:20:37 +0100

    FIxed code issue

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const pa1 = process.argv[2];
+let pa1 = process.argv[2];
 if (isNaN(pa1)) { console.log('Missing size'); } else {
+  pa1 = Number(pa1);
   for (let i = 0; i < pa1; i++) {
-    for (let j = 0; j < pa1; j++) { console.log('X'.replace('\n', '')); }
+    console.log('X'.repeat(pa1));
   }
 }
