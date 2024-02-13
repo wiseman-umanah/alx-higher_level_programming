aa59fce87d6c45983eca9d4c7d5923e09a540e3b
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-13 22:03:38 +0100

    Updated code

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
-const list = process.argv;
 const fs = require('fs');
-function getFile (fileName, hold) {
-  hold = fs.readFileSync(fileName, 'utf-8');
-  return (hold);
-}
 
-let f1, f2, f3;
-f1 = f2 = f3 = '';
-f1 = getFile(list[2], f1);
-f2 = getFile(list[3], f2);
-console.log(list);
-
-fs.writeFileSync(list[4], f1 + f2);
-f3 = getFile(list[4], f3);
-
-console.log(f3);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
