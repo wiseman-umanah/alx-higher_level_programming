f1a29a93220503831b85c59ded3def93e3ae5bb1
wiseman-umanah <wisemanumanah@gmail.com>
2024-02-18 03:50:54 +0100

    fixed up code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""a script that takes in the name of a state as an argument
-and lists all cities of that state,
-using the database hbtn_0e_4_usa"""
+"""
+Displays all cities of given state from the
+states table of database hbtn_0e_4_usa.
+Safe from SQL injections.
+Usage: ./5-filter_cities.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
+"""
 import sys
 import MySQLdb
 
-
+search = ' '.join(sys.argv[4:])
 if __name__ == "__main__":
-    args = sys.argv
-    user = args[1]
-    host = "localhost"
-    pd = args[2]
-    port = 3306
-    dbName = args[3]
-    search = ' '.join(args[4:])
-    cities = []
-
-    con = MySQLdb.connect(host=host, passwd=pd, db=dbName,
-                          user=user, port=port)
-
-    cur = con.cursor()
-    cur.execute(f"SELECT cities.name FROM cities\
-                JOIN states\
-                WHERE cities.state_id = states.id\
-                AND states.name = 'Texas'\
-                ORDER BY cities.id ASC;")
-
-    result = cur.fetchall()
-    for i in result:
-        cities.append(i[0])
-
-    print(", ".join(cities))
-
-    cur.close()
-    con.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == search]))
