7ebe18080c59aa34b532f81db89d541db42f3a6f
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-04 00:25:13 +0100

    Started new project

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Sends a request and gets value of a header
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print(r.headers["X-Request-Id"])
