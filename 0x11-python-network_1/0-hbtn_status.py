0ac28574c9c81b9ab6f9e084c477f9e440895f0b
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-03 22:34:23 +0100

    Fixed up code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from urllib import request
+"""Prints out Useful details of the web page
+and status"""
 
 
 if __name__ == "__main__":
+    from urllib import request
+
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         res = response.read()
-    print(f"Body response:\n\
-       \t- type: {type(res)}\n\
-       \t- content: {res}\n\
-       \t- utf8 content: {res.decode()}"
-       )
+    print("Body response:")
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
+    print(f"\t- utf8 content: {res.decode('utf-8')}")
