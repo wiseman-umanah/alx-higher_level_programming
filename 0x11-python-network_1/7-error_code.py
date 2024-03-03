e1a1e9f7ff0b24c65d399c504e7b73b930c82d0b
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-04 00:50:08 +0100

    Started new task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Sends a request and gets status
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    try:
+        r = requests.get(argv[1])
+        print(r.text)
+    except requests.exceptions.HTTPError as e:
+        print(r.status_code)
