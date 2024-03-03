2dda3e4e6ef0b2c3c8b83be0a055f1cbe03ecac9
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-04 00:45:54 +0100

    Updated code to meet requirement

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    r = requests.put(argv[1], data={"email": argv[2]})
+    r = requests.post(argv[1], data={"email": argv[2]})
     print(r.text)
