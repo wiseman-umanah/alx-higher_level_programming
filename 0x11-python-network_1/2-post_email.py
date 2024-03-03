c1c86846ad674a624a305626ce6302035b6565bd
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-03 23:06:54 +0100

    Started new project

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a request and data to server
+"""
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+
+    values = {"email": argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode("ascii")
+    resp = request.Request(argv[1], data)
+    with request.urlopen(resp) as response:
+        res = response.read()
+    print(res)
