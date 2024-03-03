96ac98d5e71fcbc8cceb41184cf6cfb59b1a427f
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-04 00:18:04 +0100

    Updated code to meet requirement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,9 +6,8 @@ Sends a request and gets value of a header
 if __name__ == "__main__":
     import requests
 
-    with requests.get("https://alx-intranet.hbtn.io/status") as response:
-        res = response.content
+    r = (requests.get("https://alx-intranet.hbtn.io/status")).content
     print("Body response:")
-    print(f"\t- type: {type(res)}")
-    print(f"\t- content: {res}")
-    print(f"\t- utf8 content: {res.decode('utf-8')}")
+    print(f"\t- type: {type(r)}")
+    print(f"\t- content: {r}")
+    print(f"\t- utf8 content: {r.decode('utf-8')}")
