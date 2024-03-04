2520620d2c8c7ab4e70a53fe6cc5dc6a1046e2c5
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-04 06:09:01 +0100

    Started new task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Search using a string
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    letter = "" if len(argv) == 1 else argv[1]
+    try:
+        r = requests.get("http://0.0.0.0:5000/search_user", data={"q": letter})
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print(f"[{response.get('id')}] {response.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
