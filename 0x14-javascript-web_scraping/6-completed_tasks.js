9c131befe2f90313a521ad2d185df9c73ec4e378
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-26 20:39:51 +0100

    Updated code base

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
-require('process');
-const link = process.argv[2];
-const users = {};
-request(link, function (err, status, body) {
-  if (err) console.error(err);
-  else {
-    const res = JSON.parse(body);
-    res.forEach(element => {
-      if (element.userId in users) {
-        if (element.completed) { users[element.userId] += 1; }
-      } else { users[element.userId] = 0; }
-    });
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
   }
-  console.log(users);
 });
