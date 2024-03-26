6a0ab46f3713cd133e961593aa8a623b76a86952
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-26 20:53:04 +0100

    Updated code to maintain consistency in printing out values

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,15 +6,22 @@ const link = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(link, function (err, status, body) {
   if (err) console.error(err);
   else {
-    let res = JSON.parse(body);
-    res.characters.forEach(element => {
-      request(element, function (err, status, body) {
-        if (err) console.error(err);
-        else {
-          res = JSON.parse(body);
-          console.log(res.name);
-        }
-      });
-    });
+    const res = JSON.parse(body);
+    requestCharacter(res.characters, 0);
   }
 });
+
+function requestCharacter (characters, index) {
+  // Base case: if the index is equal to the length of the characters array, stop the recursion
+  if (index >= characters.length) return;
+
+  // Make the request for the current character
+  request(characters[index], function (err, status, body) {
+    if (err) console.error(err);
+    else {
+      const res = JSON.parse(body);
+      console.log(res.name);
+    }
+    requestCharacter(characters, index + 1);
+  });
+}
